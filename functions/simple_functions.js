9a02fd22ac2a5aa498acdb091de2639eb99be7ca
Eric Schwartz <erics273@gmail.com>
2024-04-30 14:04:22 -0400

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,28 @@
+"use strict"
+
+
+//function to display my name, favorite movie, and favorite band
+function favoriteThings(){
+
+    console.log("My name is Eric");
+    console.log("My favorite movie is Almost Famous");
+    console.log("My favorite band is Metallica");
+
+}
+
+
+
+function whyImHere(){
+    console.log("I am here to teach the class and be a part of y'alls journey into tech");
+}
+
+
+function favoritePlace(){
+    console.log("I really like to visit Mr. Game & lounge in Newton, NC because they have awesome retro games for my collection");
+}
+
+
+//calling/invoking our functions
+favoriteThings();
+whyImHere();
+favoritePlace();
